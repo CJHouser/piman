66d8151e63fc18be48fca18962204fd5ea030d15
Charles Houser <cjohouser@gmail.com>
2019-11-20 20:15:33 -0800

    dhcp unicast without manual arp

--- __main__.py
+++ __main__.py
@@ -26,9 +26,9 @@ def remshell(pi_address, port_on_localhost):
     piman.remshell(pi_address, port_on_localhost)
 
 @cli.command()
-@click.argument('switch_port', nargs=1, type=click.INT)
-def powercycle(switch_port):
-    piman.powercycle(switch_port)
+@click.argument('switch_ports', nargs=-1, type=click.INT)
+def powercycle(switch_ports):
+    piman.powercycle(switch_ports)
 
 if __name__ == "__main__":
     cli()
